1fc92a202e3581e90414902785b409c969b35622
Michal Hladik <michalhladik2@seznam.cz>
2023-03-09 12:22:29 +0100

    two new images, finished for now with man hero

--- src/components/hero/MainBanner.tsx
+++ src/components/hero/MainBanner.tsx
@@ -1,11 +1,36 @@
+import useImageInfo from '@/hooks/useImageInfo'
+import Image from 'next/image'
+
 const MainBanner = () => {
+  const widthOfHero = 1290 // px
+  const heightOfHero = 580 // px
+
+  const [imageUrl, imageText] = useImageInfo()
+
   return (
-    <div className="bg-streetlaw-500 w-[1290px] h-[580px]">
-      {/* background */}
+    <div
+      // background
+      className={`bg-streetlaw-500 w-[${widthOfHero}px] h-[${heightOfHero}px] relative`}
+    >
       {/* photo */}
+      <div className="relative h-full w-auto flex items-center justify-center">
+        <Image
+          src={'/main-page-photos/' + imageUrl}
+          alt={imageText}
+          width={widthOfHero}
+          height={heightOfHero}
+          quality={100}
+          className="h-full w-auto"
+        />
+      </div>
+
       {/* top green line */}
+      <div className="bg-streetlaw-500 h-12 w-[750px] absolute top-0 left-0"></div>
       {/* bottom green line */}
-      {/* text inside bottom line */}
+      <div className="bg-streetlaw-500 h-12 w-[750px] absolute bottom-0 right-0 flex items-center justify-end">
+        {/* text inside bottom line */}
+        <h2 className="text-2xl font-semibold text-white px-5">{imageText}</h2>
+      </div>
     </div>
   )
 }
