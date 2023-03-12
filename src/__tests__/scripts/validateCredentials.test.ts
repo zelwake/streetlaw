2b040ff42db37203a1bb1adc6b8663fa2e50d922
Michal Hladik <michalhladik2@seznam.cz>
2023-03-12 14:06:10 +0100

    refactored validateCredentials function

--- src/__tests__/scripts/validateCredentials.test.ts
+++ src/__tests__/scripts/validateCredentials.test.ts
@@ -3,7 +3,7 @@ import { expect, test } from '@jest/globals'
 import { registerType } from '@projectType/authTypes'
 
 describe('Check for credentials validity', () => {
-  test('On missing information, return false and error', () => {
+  test('On missing or empty parameters, return false and error', () => {
     const body: registerType = {
       email: '',
       firstName: '',
@@ -13,7 +13,22 @@ describe('Check for credentials validity', () => {
     }
 
     expect(validateCredentials(body)).toEqual({
-      error: 'Missing parameters',
+      error: 'Nejsou vyplněny všechny údaje',
+      success: false,
+    })
+  })
+
+  test('Username being too short, return false and error', () => {
+    const body: registerType = {
+      email: 'test',
+      firstName: 'test',
+      lastName: 'test',
+      password: 'test',
+      username: 'Lojza',
+    }
+
+    expect(validateCredentials(body)).toEqual({
+      error: 'Uživatelské jméno je kratší než 6 znaků',
       success: false,
     })
   })
--- src/scripts/validateCredentials.ts
+++ src/scripts/validateCredentials.ts
@@ -4,8 +4,62 @@ export function validateCredentials(body: registerType): {
   error: string
   success: boolean
 } {
+  let error = ''
+  const success = false
+
+  const { username, password, email, firstName, lastName } = trimValues(body)
+
+  error = checkForMissingParameters(
+    username,
+    password,
+    email,
+    firstName,
+    lastName,
+    error
+  )
+
+  if (error.length > 0) return { error, success }
+
+  error = checkUsernameLength(username, error)
+
   return {
-    error: 'Missing parameters',
-    success: false,
+    error,
+    success,
+  }
+}
+
+function checkUsernameLength(username: string, error: string): string {
+  if (username.length < 6) {
+    error = 'Uživatelské jméno je kratší než 6 znaků'
+  }
+  return error
+}
+
+function checkForMissingParameters(
+  username: string,
+  password: string,
+  email: string,
+  firstName: string,
+  lastName: string,
+  error: string
+) {
+  if (!username || !password || !email || !firstName || !lastName)
+    error = 'Nejsou vyplněny všechny údaje'
+  return error
+}
+
+function trimValues(body: registerType): {
+  username: string
+  password: string
+  email: string
+  firstName: string
+  lastName: string
+} {
+  return {
+    username: body.username.trim(),
+    password: body.password.trim(),
+    email: body.email.trim(),
+    firstName: body.firstName.trim(),
+    lastName: body.lastName.trim(),
   }
 }
