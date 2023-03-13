66593e81d294e6dc43ec3f9221abc96f7446e438
Michal Hladik <michalhladik2@seznam.cz>
2023-03-13 12:24:44 +0100

    forgot to delete months in splitDate

--- src/scripts/timeDate/splitDate.ts
+++ src/scripts/timeDate/splitDate.ts
@@ -1,19 +1,4 @@
 export function splitDate(date: Date) {
-  const months = new Map<number, string>([
-    [1, 'leden'],
-    [2, 'únor'],
-    [3, 'březen'],
-    [4, 'duben'],
-    [5, 'květen'],
-    [6, 'červen'],
-    [7, 'červenec'],
-    [8, 'srpen'],
-    [9, 'září'],
-    [10, 'říjen'],
-    [11, 'listopad'],
-    [12, 'prosinec'],
-  ])
-
   const day = date.getDate()
   const month: string = date.toLocaleDateString(undefined, { month: 'long' })
   const year = date.getFullYear()
