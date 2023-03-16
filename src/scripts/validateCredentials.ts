3c3d5718847cb7e9c8a5482f0a35399550c7efcc
Michal Hladik <michalhladik2@seznam.cz>
2023-03-16 15:18:49 +0100

    updated RegisterType with removed username

--- src/scripts/validateCredentials.ts
+++ src/scripts/validateCredentials.ts
@@ -4,16 +4,15 @@ export function validateCredentials(body: RegisterType): {
   error: string | null
   success: boolean
 } {
-  const { username, password, email, firstName, lastName } = trimValues(body)
+  const { password, email, firstName, lastName } = trimValues(body)
 
-  const error = validateUser(username, password, email, firstName, lastName)
+  const error = validateUser(password, email, firstName, lastName)
 
   return { error, success: error === null }
 }
 
 function trimValues(body: RegisterType): RegisterType {
   return {
-    username: body.username.trim(),
     password: body.password.trim(),
     email: body.email.trim(),
     firstName: body.firstName.trim(),
@@ -30,21 +29,14 @@ function validatePassword(password: string): string | null {
 }
 
 function validateUser(
-  username: string,
   password: string,
   email: string,
   firstName: string,
   lastName: string
 ): string | null {
-  const missingParams =
-    !username || !password || !email || !firstName || !lastName
+  const missingParams = !password || !email || !firstName || !lastName
   if (missingParams) {
     return 'Nejsou vyplněny všechny údaje'
-  }
-
-  const invalidUsername = username.length < 6
-  if (invalidUsername) {
-    return 'Uživatelské jméno je kratší než 6 znaků'
   }
 
   const invalidPassword = validatePassword(password)
