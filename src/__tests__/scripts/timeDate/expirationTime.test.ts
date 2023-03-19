9866c285def3b03eafd59628ae40b25a42f278ea
Michal Hladik <michalhladik2@seznam.cz>
2023-03-19 13:30:53 +0100

    timeDate tests

---
+++ src/__tests__/scripts/timeDate/expirationTime.test.ts
@@ -0,0 +1,33 @@
+import { isExpired, setExpirationDate } from '@/scripts/timeDate/expirationTime'
+import { expect, it } from '@jest/globals'
+
+describe('Testing setExpirationDate function', () => {
+  it('should create new time that is two hours in future', () => {
+    const futureTime = new Date()
+    futureTime.setHours(futureTime.getHours() + 2)
+
+    expect(setExpirationDate()).toStrictEqual(futureTime)
+  })
+
+  it('should not equal since times are not same', () => {
+    const nowTime = new Date()
+
+    expect(setExpirationDate()).not.toStrictEqual(nowTime)
+  })
+})
+
+describe('Testing isExpired function', () => {
+  it('should return false when two hours has not yet passed', () => {
+    const time = new Date()
+    time.setMinutes(time.getMinutes() + 10)
+
+    expect(isExpired(time)).toBe(false)
+  })
+
+  it('should return true since two hours has already passed', () => {
+    const time = new Date()
+    time.setMinutes(time.getMinutes() - 10)
+
+    expect(isExpired(time)).toBe(true)
+  })
+})
