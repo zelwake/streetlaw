b1bf3628aa2a968490c08bc3cccee0a71d0a173c
Michal Hladik <michalhladik2@seznam.cz>
2023-03-19 12:59:27 +0100

    fixed email payload text

--- src/scripts/createMailPayload.ts
+++ src/scripts/createMailPayload.ts
@@ -6,9 +6,10 @@ export function createMailPayload(
   return {
     from: 'noreply@streetlaw.eu',
     to: email,
-    subject: 'Zpráva od noreply.registrace',
-    html: `<h1>Děkujeme za registraci</h1>
-        <p>Pro její dokončení klikněte na odkaz níže</p>
-        <p><a href='${process.env.WEBSITE_URL}/auth/confirmation?id=${id}&token=${emailVerificationHash}'>Potvrzení emailu</a></p>`,
+    subject: 'Zpráva od registrace STREET LAW',
+    text: `Děkujeme za registrace! Pro její dokončení klikněte na odkaz níže: ${process.env.WEBSITE_URL}/auth/confirmation?id=${id}&token=${emailVerificationHash} Dokončení registrace`,
+    html: `<h1>Děkujeme za registraci!</h1>
+        <p>Pro její dokončení klikněte na odkaz níže:</p>
+        <p><a href='${process.env.WEBSITE_URL}/auth/confirmation?id=${id}&token=${emailVerificationHash}'>Dokončení registrace</a></p>`,
   }
 }
