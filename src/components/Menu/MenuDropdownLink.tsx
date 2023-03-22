8efbd0ed96270b7c92f1c8093ac05f1a600b17bb
Michal Hladik <michalhladik2@seznam.cz>
2023-03-22 20:00:12 +0100

    profile form WIP

--- src/components/Menu/MenuDropdownLink.tsx
+++ src/components/Menu/MenuDropdownLink.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 const MenuDropdownLink = ({ text, link }: { text: string; link: string }) => {
   return (
     <Link href={link}>
-      <p className="nav-btw-dropdown hover:bg-streetlaw-500 hover:text-white">
+      <p className="nav-btn-dropdown hover:bg-streetlaw-500 hover:text-white">
         {text}
       </p>
     </Link>
--- src/pages/settings/index.tsx
+++ src/pages/settings/index.tsx
@@ -31,26 +31,50 @@ const Profile = () => {
                 key={item.slug}
                 className={
                   item.slug === slug
-                    ? 'h-9 pl-5 py-1 text-xl cursor-pointer bg-streetlaw-500 text-white'
-                    : 'h-9 pl-5 py-1 text-xl cursor-pointer hover:font-bold'
+                    ? 'h-9 pl-5 mr-3 py-1 text-xl cursor-pointer bg-streetlaw-500 text-white relative'
+                    : 'h-9 pl-5 py-1 text-xl cursor-pointer hover:bg-streetlaw-500 hover:text-white relative'
                 }
                 onClick={() => setSlug(item.slug)}
               >
+                {item.slug === slug && (
+                  <div
+                    className="w-0 h-0 
+  border-t-[18px] border-t-transparent
+  border-l-[12px] border-l-streetlaw-500
+  border-b-[18px] border-b-transparent
+  absolute -right-3 top-0"
+                  ></div>
+                )}
                 {item.name}
               </li>
             ))}
           </ul>
-          <div>
-            <form>
-              <label htmlFor="email">Email</label>
-              <input id="email" type="email" />
-              <label htmlFor="name">Jméno</label>
-              <input id="name" type="text" />
-              <label htmlFor="surname">Příjmení</label>
-              <input id="surname" type="text" />
-              <label htmlFor="about">Popis</label>
-              <input id="about" type="textarea" />
-              <label htmlFor="photo">Fotografie</label>
+          <div className="border-l-[1px] w-[1085px] border-black">
+            <form className="grid grid-cols-5 items-center pl-9 pr-16 mt-7 gap-7">
+              <label htmlFor="email" className="profile-label">
+                Email:
+              </label>
+              <input id="email" type="email" className="profile-input" />
+              <label htmlFor="name" className="profile-label">
+                Jméno:
+              </label>
+              <input id="name" type="text" className="profile-input" />
+              <label htmlFor="surname" className="profile-label">
+                Příjmení:
+              </label>
+              <input id="surname" type="text" className="profile-input" />
+              <label htmlFor="about" className="profile-label place-self-start">
+                Popis:
+              </label>
+              <textarea
+                id="about"
+                cols={64}
+                rows={10}
+                className="col-span-4 resize-none overflow-y-scroll outline-none py-1 px-3 text-2xl bg-gray-100"
+              ></textarea>
+              <label htmlFor="photo" className="profile-label">
+                Fotografie:
+              </label>
               <input type="file" />
             </form>
           </div>
