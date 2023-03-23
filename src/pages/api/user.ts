436dd32d2201ce136632404dcc6d0952e7f7db7f
Michal Hladik <michalhladik2@seznam.cz>
2023-03-23 17:49:36 +0100

    update is saved inside database

--- src/pages/api/user.ts
+++ src/pages/api/user.ts
@@ -1,3 +1,4 @@
+import prisma from '@/lib/prisma'
 import {
   UpdateFormInterface,
   UserPUTInterface,
@@ -13,21 +14,36 @@ export default async function handler(
 
   if (!token) return res.status(401).json({ data: 'Unauthorized' })
 
-  const {
-    email,
-    firstName,
-    lastName,
-    photoUrl,
-    description,
-  }: UpdateFormInterface = req.body
-
-  console.log(email)
-
-  // TODO validate data
-
   switch (req.method) {
     case 'PUT':
-      return res.status(200).json({ data: 'Success' })
+      const {
+        email,
+        firstName,
+        lastName,
+        photoUrl,
+        description,
+      }: UpdateFormInterface = req.body
+
+      // TODO validate data
+
+      try {
+        await prisma.user.update({
+          where: {
+            id: token.sub,
+          },
+          data: {
+            email,
+            firstName,
+            lastName,
+            photoUrl,
+            description,
+          },
+        })
+        return res.status(200).json({ data: 'Změněno.' })
+      } catch (error) {
+        console.log(error)
+        return res.status(200).json({ data: 'Chyba na straně serveru.' })
+      }
     default:
       return res.status(405).json({ data: 'Method not allowed' })
   }
