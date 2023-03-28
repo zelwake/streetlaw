966564857ad6f39c3f3cbf7929fd3633f3fda1c6
Michal Hladik <michalhladik2@seznam.cz>
2023-03-28 17:45:39 +0200

    post endpoint to create new relation

--- src/pages/api/settings/lesson/index.ts
+++ src/pages/api/settings/lesson/index.ts
@@ -8,38 +8,52 @@ export default async function handler(
   const { method } = req
 
   switch (method) {
-    case 'GET':
-      {
-        const { category } = req.query
+    case 'GET': {
+      const { category } = req.query
 
-        if (!category) return res.status(400).json({ data: 'Bad request' })
+      if (!category) return res.status(400).json({ data: 'Bad request' })
 
-        try {
-          const categoryData = await prisma.keyword_lesson_category.findFirst({
-            where: {
-              id: parseInt(category as string),
-            },
-            include: {
-              keywords: {
-                select: {
-                  Keyword: true,
-                },
+      try {
+        const categoryData = await prisma.keyword_lesson_category.findFirst({
+          where: {
+            id: parseInt(category as string),
+          },
+          include: {
+            keywords: {
+              select: {
+                Keyword: true,
               },
             },
-          })
+          },
+        })
 
-          if (!categoryData) return res.status(404).json({ data: 'Not found' })
+        if (!categoryData) return res.status(404).json({ data: 'Not found' })
 
-          res.status(200).json({ data: categoryData })
-        } catch (error) {
-          console.log(error)
-          return res.status(500).json({ data: 'Internal server error' })
-        }
+        return res.status(200).json({ data: categoryData })
+      } catch (error) {
+        console.log(error)
+        return res.status(500).json({ data: 'Internal server error' })
       }
-      break
+    }
     case 'POST': {
+      const { keyword, category }: { keyword: number; category: number } =
+        req.body
+
+      try {
+        const addRelation = await prisma.keyword_to_lesson_category.create({
+          data: {
+            categoryId: category,
+            keywordId: keyword,
+          },
+        })
+
+        return res.status(201).json({ data: addRelation })
+      } catch (error) {
+        console.log(error)
+        return res.status(500).json({ data: 'Internal server error' })
+      }
     }
   }
 
-  res.end()
+  res.status(405).json({ data: 'Method not allowed' })
 }
