587d898ed9315b8f3e4b0c9a862d805b9a978814
Michal Hladik <michalhladik2@seznam.cz>
2023-04-05 15:50:45 +0200

    install success?

---
+++ postinstall.js
@@ -0,0 +1,10 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const fse = require('fs-extra')
+const path = require('path')
+const topDir = __dirname
+fse.emptyDirSync(path.join(topDir, 'public', 'tinymce'))
+fse.copySync(
+  path.join(topDir, 'node_modules', 'tinymce'),
+  path.join(topDir, 'public', 'tinymce'),
+  { overwrite: true }
+)
